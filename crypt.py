2fead6b0929de6bdf95444bf45187e8425e8bab5
No comm <67383667+no-comm@users.noreply.github.com>
2022-09-08 21:50:56 +0500

    Add files via upload

--- crypt.py
+++ crypt.py
@@ -1,5 +1,4 @@
 from hashlib import md5
 def encrypt(text: str, salt: str):
-	s = text+str(md5(f"{text+salt}".encode()))
-	return md5(f"{salt+f'{md5(text.encode())}'}".encode()).hexdigest()
+	return md5(f"{salt+text+salt}".encode()).hexdigest()
 print(encrypt("gg bro", "1245683294"))
